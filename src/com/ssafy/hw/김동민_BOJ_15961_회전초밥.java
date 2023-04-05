9641f5a1f08821b5fb799afeab4ec918a47c8a70
김동민 <anwjr41@naver.com>
2023-04-06 01:39:57 +0900

    04-06 1:39 hw 회전초밥

--- src/com/ssafy/hw/김동민_BOJ_15961_회전초밥.java
+++ src/com/ssafy/hw/김동민_BOJ_15961_회전초밥.java
@@ -1,4 +1,4 @@
-package com.ssafy.ws.BOJ.Gold;
+package com.ssafy.hw;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class G4_15961_회전초밥 {
+public class 김동민_BOJ_15961_회전초밥 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
