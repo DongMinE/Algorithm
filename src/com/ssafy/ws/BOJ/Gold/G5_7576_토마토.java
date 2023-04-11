6d707c230ca7c51841ff4ed9fb76fc992d7e63ab
김동민 <anwjr41@naver.com>
2023-04-12 00:54:07 +0900

    04-12 12:53 algo 스터디

--- src/com/ssafy/ws/BOJ/Gold/G5_7576_토마토.java
+++ src/com/ssafy/ws/BOJ/Gold/G5_7576_토마토.java
@@ -10,7 +10,9 @@ import java.util.StringTokenizer;
 public class G5_7576_토마토 {
 	static int m, n, res, arr[][];
 	static int[] dr = { 0, 1, 0, -1 }, dc = { 1, 0, -1, 0 };
-	static boolean[][] visited;
+	static Queue<int[]> que = new LinkedList<>();
+	static int max = Integer.MIN_VALUE;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,49 +20,60 @@ public class G5_7576_토마토 {
 		m = Integer.parseInt(st.nextToken());
 
 		arr = new int[m][n];
-		visited = new boolean[m][n];
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < n; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
+				if (arr[i][j] == 1)
+					que.add(new int[] { i, j });
 			}
 		}
-//		for (int[] a : arr) {
-//			System.out.println(Arrays.toString(a));
-//		}
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (!visited[i][j] && arr[i][j] == 1) {
-					bfs(i, j);
-				}
-			}
-		}
-		System.out.println(res);
+		bfs();
+		System.out.println(max);
 
 	}
-	private static void bfs(int a, int b) {
-		Queue<int[]> que = new LinkedList<>();
-		que.add(new int[] {a,b});
-		visited[a][b] = true;
-		res++;
-		
-		while(!que.isEmpty()) {
+
+	private static void bfs() {
+		while (!que.isEmpty()) {
 			int[] num = que.poll();
-			
+
 			for (int i = 0; i < 4; i++) {
 				int newa = num[0] + dr[i];
 				int newb = num[1] + dc[i];
-				
+
 				if (newa < m && newb < n && newa >= 0 && newb >= 0) {
-					if (arr[newa][newb] == 0 && !visited[newa][newb]) {
-						visited[newa][newb] = true;
-						arr[newa][newb] = 1;
-						bfs(newa, newb);
+					if (arr[newa][newb] == 0) {
+						arr[newa][newb] = arr[num[0]][num[1]] + 1;
+						que.add(new int[] { newa, newb });
 					}
 				}
 			}
-			
-			
 		}
+		countDay();
+	}
+
+	private static void countDay() {
+		if (check())
+			max = -1;
+		else {
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					if (max < arr[i][j]) {
+						max = arr[i][j];
+					}
+				}
+			}
+			max = max - 1;
+		}
+	}
+
+	private static boolean check() {
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i][j] == 0)
+					return true;
+			}
+		}
+		return false;
 	}
 }
