58c15541b02f9af2a514b516e243ed55db01a5ef
김동민 <anwjr41@naver.com>
2023-12-22 12:10:55 +0900

    12-22 알고 다시 시작

--- src/com/ssafy/offline/day04/모닝순조부.java
+++ src/com/ssafy/offline/day04/모닝순조부.java
@@ -4,82 +4,91 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class 모닝순조부 {
-	static int N, R;
-	static int[] numbers, input;
-	static boolean[] selected;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		R = sc.nextInt();
-		numbers = new int[R];
-		input = new int[N];
-		selected = new boolean[N];
+    static int N, R;
+    static int[] numbers, input;
+    static boolean[] selected;
 
-		for (int i = 0; i < N; i++)
-			input[i] = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        R = sc.nextInt();
+        numbers = new int[R];
+        input = new int[N];
+        selected = new boolean[N];
 
-		System.out.println("순열");
-		perm(0);
-		System.out.println("조합");
-		comb(0, 0);
-		System.out.println("부분집합 - 재귀");
-		subset(0);
-		System.out.println("부분집합 - 비트");
-		sebset();
-	}
-
-	private static void sebset() {
-		for (int i = 0; i < (1 << N); i++) {
-			//System.out.print("(i : " + i + ")");
-			for (int j = 0; j < N; j++) {
-				//System.out.print("(j : " + j + ")");
-				if ((i & (1 << j)) == 0) continue;
-				System.out.print( input[j] + " ");
-			}
-			System.out.println();
-		}
-	}
-
-	private static void subset(int cnt) {
-		if (cnt == N) {
-			for (int i = 0; i <N; i++) {
-				if (!selected[i]) continue;
-				System.out.print(input[i] + " ");
-			}
-			System.out.println();
-			return;
-		}
-		selected[cnt] = true;
-		subset(cnt+1);
-		selected[cnt] = false;
-		subset(cnt+1);
-	}
-
-	private static void comb(int cnt, int start) {
-		if (cnt == R) {
-			System.out.println(Arrays.toString(numbers));
-			return;
-		}
-		for (int i = start; i < N; i++) {
-			numbers[cnt] = input[i];
-			comb(cnt+1,i+1);
-		}
-	}
-
-	private static void perm(int cnt) {
-		if (cnt == R) {
-			//System.out.println("순열");
-			System.out.println(Arrays.toString(numbers));
-			return;
-		}
 		for (int i = 0; i < N; i++) {
-			if (selected[i]) continue;
-			selected[i] = true;
-			numbers[cnt] = input[i];
-			perm(cnt+1);
-			selected[i] = false;
+			input[i] = sc.nextInt();
 		}
-	}
-	
+
+        System.out.println("순열");
+        perm(0);
+        System.out.println("조합");
+        comb(0, 0);
+        System.out.println("부분집합 - 재귀");
+        subset(0);
+        System.out.println("부분집합 - 비트");
+        sebset();
+
+    }
+
+    private static void sebset() {
+        for (int i = 0; i < (1 << N); i++) {
+            //System.out.print("(i : " + i + ")");
+            for (int j = 0; j < N; j++) {
+                //System.out.print("(j : " + j + ")");
+				if ((i & (1 << j)) == 0) {
+					continue;
+				}
+                System.out.print(input[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void subset(int cnt) {
+        if (cnt == N) {
+            for (int i = 0; i < N; i++) {
+				if (!selected[i]) {
+					continue;
+				}
+                System.out.print(input[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        selected[cnt] = true;
+        subset(cnt + 1);
+        selected[cnt] = false;
+        subset(cnt + 1);
+    }
+
+    private static void comb(int cnt, int start) {
+        if (cnt == R) {
+            System.out.println(Arrays.toString(numbers));
+            return;
+        }
+        for (int i = start; i < N; i++) {
+            numbers[cnt] = input[i];
+            comb(cnt + 1, i + 1);
+        }
+    }
+
+    private static void perm(int cnt) {
+        if (cnt == R) {
+            //System.out.println("순열");
+            System.out.println(Arrays.toString(numbers));
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+			if (selected[i]) {
+				continue;
+			}
+            selected[i] = true;
+            numbers[cnt] = input[i];
+            perm(cnt + 1);
+            selected[i] = false;
+        }
+    }
+
 }
