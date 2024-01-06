5782de93107309ce12f60faa9fb1c00ab86bdcf8
김동민 <anwjr41@naver.com>
2024-01-06 22:34:10 +0900

    1ㅇ1일1알고 24-01-06

---
+++ src/com/ssafy/ws/BOJ/Silver/S3_1966_프린터큐.java
@@ -0,0 +1,24 @@
+package com.ssafy.ws.BOJ.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Queue;
+
+public class S3_1966_프린터큐 {
+
+    static StringBuilder sb;
+    static Queue<Integer> q;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)
+        );
+        sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+
+
+        }
+
+    }
+}
