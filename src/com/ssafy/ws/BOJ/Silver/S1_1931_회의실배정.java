89e2a0d7e67158d8e8f1ed49b33a1ab6778acbb1
김동민 <anwjr41@naver.com>
2024-02-15 03:42:16 +0900

    02-14 알고

---
+++ src/com/ssafy/ws/BOJ/Silver/S1_1931_회의실배정.java
@@ -0,0 +1,42 @@
+package com.ssafy.ws.BOJ.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class S1_1931_회의실배정 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        int[][] time = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+            return o1[1] - o2[1];
+        });
+        int endTime = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (endTime <= time[i][0]) {
+                endTime = time[i][1];
+                cnt++;
+            }
+
+        }
+        System.out.println(cnt);
+    }
+}
