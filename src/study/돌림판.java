ffb16b8560eaa2d8cea18a6a9fb587dc57add9fe
김동민 <anwjr41@naver.com>
2024-03-01 02:03:05 +0900

    02-29 돌림판수정, 알고

--- src/study/돌림판.java
+++ src/study/돌림판.java
@@ -3,16 +3,12 @@ package study;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Random;
-import java.util.Set;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.util.*;
 
 public class 돌림판 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         while (true) {
             System.out.print("뽑을 숫자의 범위를 입력해주세요: ");
@@ -20,7 +16,10 @@ public class 돌림판 {
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
 
-            System.out.print("뽑을 개수를 입력해주세요: ");
+            System.out.print("진행할 인원을 입력하세요: ");
+            int l = Integer.parseInt(br.readLine());
+
+            System.out.print("질문 개수를 입력해주세요: ");
             int k = Integer.parseInt(br.readLine());
 
             if (m - n + 1 < k) {
@@ -30,23 +29,31 @@ public class 돌림판 {
             }
 
             Random random = new Random();
-            Set<Integer> set = new TreeSet<>();
-            System.out.println();
-            System.out.println("과연~~!!! 당신이 답 할 질문은?!");
 
-            for (int i = 0; i < k; i++) {
-                int num;
-                do {
-                    num = n + random.nextInt(m - n + 1);
-                } while (set.contains(num));
-                set.add(num);
+            for (int cycle = 1; cycle <= k; cycle++) {
+                List<Integer> numbers = new ArrayList<>();
+                System.out.println();
+                System.out.println("과연~~!!! 당신이 답 할 질문은?! (문제 " + cycle + ")");
+
+                for (int i = 1; i <= l; i++) {
+                    int num;
+                    do {
+                        num = n + random.nextInt(m - n + 1);
+                    } while (numbers.contains(num));
+                    numbers.add(num);
+                    System.out.print("사람 " + i + ": " + num);
+                    if (i != 5) {
+                        br.readLine();
+                    }
+                }
+
+                if (cycle != k) {
+                    System.out.println("엔터 누르면 다음 문제...");
+                    br.readLine();
+                }
             }
 
-            for (int num : set) {
-                sb.append(num).append(" ");
-            }
-            System.out.println(sb);
-            System.out.println("축하드립니다!!! (●ó⌓ò●)");
+            System.out.println("축하드립니다!!! 오늘의 스터디가 끝났습니다. (●ó⌓ò●)");
             break;
         }
     }
