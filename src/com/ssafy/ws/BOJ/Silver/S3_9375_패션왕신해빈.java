9ea9632d844455292cb36c16b9523d97bf1be471
김동민 <anwjr41@naver.com>
2024-03-06 18:13:07 +0900

    0306 알고

---
+++ src/com/ssafy/ws/BOJ/Silver/S3_9375_패션왕신해빈.java
@@ -0,0 +1,18 @@
+package com.ssafy.ws.BOJ.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S3_9375_패션왕신해빈 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            
+        }
+    }
+}
