cb6731f311590de045b5a9b75c6af6525d4d529b
김동민 <anwjr41@naver.com>
2024-03-14 01:08:45 +0900

    알고 0314

--- src/com/ssafy/ws/BOJ/Silver/S3_10451_순열사이클.java
+++ src/com/ssafy/ws/BOJ/Silver/S3_10451_순열사이클.java
@@ -3,6 +3,9 @@ package com.ssafy.ws.BOJ.Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class S3_10451_순열사이클 {
@@ -13,9 +16,30 @@ public class S3_10451_순열사이클 {
         int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            int[][] list = new int[n + 1][n + 1];
+            boolean[] check = new boolean[n + 1];
+            int[] list = new int[n + 1];
+            int cnt = 0;
+
             st = new StringTokenizer(br.readLine());
-            
+            for (int i = 1; i <= n; i++) {
+                list[i] = Integer.parseInt(st.nextToken());
+            }
+            for (int i = 1; i <= n; i++) {
+                if (!check[i]) {
+                    check[i] = true;
+                    int next = list[i];
+
+                    while (true) {
+                        if (check[next]) {
+                            cnt++;
+                            break;
+                        }
+                        check[next] = true;
+                        next = list[next];
+                    }
+                }
+            }
+            System.out.println(cnt);
         }
     }
 }
