b407d22f4dfdc507374a8d395a273dddc36ec1c2
김동민 <anwjr41@naver.com>
2024-03-13 18:10:23 +0900

    0313 알고

---
+++ src/com/ssafy/ws/BOJ/Silver/S3_10451_순열사이클.java
@@ -0,0 +1,21 @@
+package com.ssafy.ws.BOJ.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S3_10451_순열사이클 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] list = new int[n + 1][n + 1];
+            st = new StringTokenizer(br.readLine());
+            
+        }
+    }
+}
