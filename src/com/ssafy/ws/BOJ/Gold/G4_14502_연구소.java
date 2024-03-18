579463cfb8041b20e9a7ab684a9e517092d25559
김동민 <anwjr41@naver.com>
2024-03-19 02:59:18 +0900

    0317 14502 연구소

--- src/com/ssafy/ws/BOJ/Gold/G4_14502_연구소.java
+++ src/com/ssafy/ws/BOJ/Gold/G4_14502_연구소.java
@@ -3,12 +3,14 @@ package com.ssafy.ws.BOJ.Gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class G4_14502_연구소 {
 
     static int[][] list;
+    static ArrayList<int[]> zero;
     static int shall = 0;
 
     public static void main(String[] args) throws IOException {
@@ -16,11 +18,25 @@ public class G4_14502_연구소 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+
+        // 원본배열
         list = new int[n][m];
+        // 0이 담긴 좌표 배열
+        zero = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            list[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                list[i][j] = Integer.parseInt(st.nextToken());
+                if (list[i][j] == 0) {
+                    zero.add(new int[]{i, j});
+                }
+            }
         }
-        
+        comb();
+    }
+
+    private static void comb() {
+
     }
 }
