88bbe1bea7d59228fa6c446fc648d6ab5c9e540d
김동민 <anwjr41@naver.com>
2024-03-20 00:54:56 +0900

    0320 연구소 14502

--- src/com/ssafy/ws/BOJ/Gold/G4_14502_연구소.java
+++ src/com/ssafy/ws/BOJ/Gold/G4_14502_연구소.java
@@ -17,9 +17,10 @@ public class G4_14502_연구소 {
     static int[][] virus = new int[3][2];
     static int shall = 0;
     static int n, m;
-    static int[] nr = new int[]{0, 0, 1, -1};
-    static int[] nx = new int[]{-1, 1, 0, -0};
+    static int[] nx = new int[]{0, 0, 1, -1};
+    static int[] ny = new int[]{-1, 1, 0, -0};
     static Queue<int[]> q = new ArrayDeque<>();
+    static int resSafeCnt = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -44,6 +45,8 @@ public class G4_14502_연구소 {
         check = new boolean[zero.size()];
 
         comb(0, 0);
+
+        System.out.println(resSafeCnt);
     }
 
     private static void comb(int idx, int cnt) {
@@ -53,7 +56,9 @@ public class G4_14502_연구소 {
         } else {
             for (int i = idx; i < zero.size(); i++) {
                 virus[cnt] = zero.get(i);
+                list[virus[cnt][0]][virus[cnt][1]] = 1;
                 comb(i + 1, cnt + 1);
+                list[virus[cnt][0]][virus[cnt][1]] = 0;
             }
         }
     }
@@ -67,11 +72,48 @@ public class G4_14502_연구소 {
                 }
             }
         }
+        int copyList[][] = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            copyList[i] = list[i].clone();
+        }
+
         while (!q.isEmpty()) {
             int[] current = q.poll();
             int x = current[0];
             int y = current[1];
 
+            for (int i = 0; i < 4; i++) {
+                int dx = nx[i] + x;
+                int dy = ny[i] + y;
+
+                if (dx >= 0 && dx < n && dy >= 0 && dy < m) {
+                    if (copyList[dx][dy] == 0) {
+                        copyList[dx][dy] = 2;
+
+                        q.add(new int[]{dx, dy});
+                    }
+                }
+            }
+        }
+//        for (int[] arr : copyList) {
+//            System.out.println(Arrays.toString(arr));
+//        }
+//        System.out.println("----------------------------------");
+        safeZone(copyList);
+
+    }
+
+    private static void safeZone(int[][] copyList) {
+        int safeCnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (copyList[i][j] == 0) {
+                    safeCnt++;
+                }
+            }
+        }
+        if (safeCnt > resSafeCnt) {
+            resSafeCnt = safeCnt;
         }
     }
 }
