f23962eec9edd0963155caed203167b4a4926b0b
김동민 <anwjr41@naver.com>
2024-03-19 18:20:52 +0900

    0319 연구소

--- src/com/ssafy/ws/BOJ/Gold/G4_14502_연구소.java
+++ src/com/ssafy/ws/BOJ/Gold/G4_14502_연구소.java
@@ -3,21 +3,29 @@ package com.ssafy.ws.BOJ.Gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class G4_14502_연구소 {
 
     static int[][] list;
+    static boolean[] check;
     static ArrayList<int[]> zero;
+    static int[][] virus = new int[3][2];
     static int shall = 0;
+    static int n, m;
+    static int[] nr = new int[]{0, 0, 1, -1};
+    static int[] nx = new int[]{-1, 1, 0, -0};
+    static Queue<int[]> q = new ArrayDeque<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
         // 원본배열
         list = new int[n][m];
@@ -33,10 +41,37 @@ public class G4_14502_연구소 {
                 }
             }
         }
-        comb();
+        check = new boolean[zero.size()];
+
+        comb(0, 0);
     }
 
-    private static void comb() {
+    private static void comb(int idx, int cnt) {
+        if (cnt == 3) {
+            //정해진 위치 3개를 갖고 바이러스 퍼지기 시작
+            spread(virus);
+        } else {
+            for (int i = idx; i < zero.size(); i++) {
+                virus[cnt] = zero.get(i);
+                comb(i + 1, cnt + 1);
+            }
+        }
+    }
 
+    // 바이러스 퍼뜨려
+    private static void spread(int[][] virus) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (list[i][j] == 2) {
+                    q.add(new int[]{i, j});
+                }
+            }
+        }
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+            int x = current[0];
+            int y = current[1];
+
+        }
     }
 }
