f2aaae6199aa5416c015a2ee5fca645035b35e9e
김동민 <anwjr41@naver.com>
2024-03-28 01:42:26 +0900

    0327 알고

---
+++ src/com/ssafy/ws/BOJ/Silver/S3_17626_foursquares.java
@@ -0,0 +1,25 @@
+package com.ssafy.ws.BOJ.Silver;
+
+import java.io.*;
+import java.util.*;
+
+public class S3_17626_foursquares {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, dp[i - j * j]);
+            }
+            dp[i] = min + 1;
+        }
+        System.out.println(dp[n]);
+    }
+}
