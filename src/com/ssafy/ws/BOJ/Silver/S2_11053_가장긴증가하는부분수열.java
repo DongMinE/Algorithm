54c9f3161465ab931b69128b7a0e96bb417dd96d
김동민 <anwjr41@naver.com>
2024-06-24 22:50:30 +0900

    0624 알고

--- src/com/ssafy/ws/BOJ/Silver/S2_11053_가장긴증가하는부분수열.java
+++ src/com/ssafy/ws/BOJ/Silver/S2_11053_가장긴증가하는부분수열.java
@@ -8,18 +8,34 @@ import java.util.StringTokenizer;
 
 public class S2_11053_가장긴증가하는부분수열 {
 
-    static int[] list;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        list = new int[n];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            list[i] = Integer.parseInt(st.nextToken());
+
+        int N = Integer.parseInt(br.readLine());
+        int[] seq = new int[N];
+        int[] dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(Arrays.toString(list));
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (seq[i] > seq[j] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int max = -1;
+        for (int i = 0; i < N; i++) {
+            max = Math.max(dp[i], max);
+        }
+        System.out.println(max);
+
     }
+
 }
